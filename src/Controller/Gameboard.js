dea2b3753405fce67763b382b51dac9d27005ab3
Preslav977 <p.cvetanow@gmail.com>
2023-10-20 14:25:54 +0300

    Created two tests for placing a ship, first if rows and cols are empty and the opposite

--- src/Controller/Gameboard.js
+++ src/Controller/Gameboard.js
@@ -1,22 +1,34 @@
 const battleShipBoard = (() => {
   const gameBoard = () => {
-    const createBoard = () => {
-      const cols = 8;
-      const rows = 8;
-      const board = [];
+    const cols = 8;
+    const rows = 8;
+    const board = [];
 
-      for (let i = 0; i < cols; i += 1) {
-        board[i] = [];
-        for (let j = 0; j < rows; j += 1) {
-          board[i][j] = "";
-        }
+    for (let i = 0; i < cols; i += 1) {
+      board[i] = [];
+      for (let j = 0; j < rows; j += 1) {
+        board[i][j] = "";
       }
-      return board;
+    }
+
+    const placeShip = (col, row, ship, direction) => {
+      if (board[col][row] === "") {
+        board[col][row] = ship;
+        return true;
+      }
+      return false;
+      // check if coordinates are empty
+      // call the method that creates new object from Ship
+      // pass the arguments for the ship either directly in function
+      // or in placeShip argument hmm
+      // prevent ships from overlapping
+      // figure out the direction on how to place the ships on X or Y axis
+      // figure out how to do this 5 times for each ship either using a loop
+      // or maybe using recursion for recursion base case could be if all 5 ships
+      // has been placed return else keep going but don't place on top of each other
     };
 
-    const placeShip = (row, col, ship, direction) => {};
-
-    return { createBoard };
+    return { placeShip };
   };
 
   return {
--- src/Controller/Gameboard.test.js
+++ src/Controller/Gameboard.test.js
@@ -1,1 +1,16 @@
 const battleShipBoard = require("./Gameboard");
+
+test("Check if board coordinates are empty, before placing ship", () => {
+  const playerBoard = battleShipBoard.gameBoard();
+  expect(playerBoard.placeShip(1, 0, "carrier")).toBe(true);
+});
+
+test("Check if ship is not overlapping with another ship", () => {
+  const playerBoard = battleShipBoard.gameBoard();
+  expect(playerBoard.placeShip(1, 0, "carrier")).toBe(true);
+  expect(playerBoard.placeShip(1, 0, "carrier")).toBe(false);
+});
+
+// test("Check if ship is placed in X axis on the board", () => {});
+
+// test("Check if ship is placed in Y axis on the board", () => {});
